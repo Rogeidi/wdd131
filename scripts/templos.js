4db29b579fab5a865cd8ef4e87aacb3111753cb1
Rogeidi <rogeidivirtual@gmail.com>
2026-07-15 06:14:48 -0300

    álbum-do-templo

--- scripts/templos.js
+++ scripts/templos.js
@@ -2,7 +2,7 @@ const anoAtualSpan = document.getElementById("anoatual");
 anoAtualSpan.textContent = new Date().getFullYear();
 
 const ultimaModificacaoP = document.getElementById("ultimaModificacao");
-ultimaModificacaoP.textContent = 'última Modificação: ${document.lastModified}';
+ultimaModificacaoP.textContent = `última Modificação: ${document.lastModified}`;
 
 const menuButton = document.getElementById("menu-button");
 const menuNav = document.getElementById("menu-nav");
